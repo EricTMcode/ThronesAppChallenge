29ebb1373874ff62076a8dfe6bebc509fc3eb8e2
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-05-02 13:28:37 +0200

    Initial Commit

---
+++ ThronesAppChallenge/ThronesAppChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThronesAppChallengeApp.swift
+//  ThronesAppChallenge
+//
+//  Created by Eric on 02/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct ThronesAppChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
